7a7fa40fb865af4ed30efb0ee11c5cbb9c05b053
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-01-18 16:14:47 -0700

    moved to isolated repo

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+import "./App.css"
+import { Header } from "./Header.js"
+import { Listings } from "./Section.js"
+import { Form } from "./Form.js"
+
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+    this.updateData = this.updateData.bind(this)
+  }
+
+  componentDidMount() {
+    fetch("./listings.json")
+      .then(resp => resp.json())
+      .then(resp => this.setState({ data: resp }))
+  }
+
+  updateData(data) {
+    let newData = this.state.data
+    newData.unshift(data)
+    return this.setState({data:newData})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <Listings listings={this.state.data} />
+          <Form updateData={this.updateData} />
+        </main>
+      </div>
+    )
+  }
+}
