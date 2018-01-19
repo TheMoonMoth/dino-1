3154c8837bc73c6a58bdf72e77ba78aecbcb402f
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-01-19 10:37:43 -0700

    Added footer and put cypress on .gitignore

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export class Footer extends React.Component {
+  render() {
+    return <footer className="App-footer">
+              <p><small>Copyright &copy; 2017</small></p>
+          </footer>
+  }
+}
